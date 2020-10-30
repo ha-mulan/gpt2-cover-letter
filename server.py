e0281bf0a1f9d1c9f9dc986706a0456e42595bc4
Jeong-Hyun-Su <42924998+Jeong-Hyun-Su@users.noreply.github.com>
2020-10-30 19:48:34 +0900

    Update server.py

--- server.py
+++ server.py
@@ -15,7 +15,8 @@ CHECK_INTERVAL = 0.1
 
 tokenizer = AutoTokenizer.from_pretrained("jonasmue/cover-letter-gpt2")
 model = AutoModelWithLMHead.from_pretrained("jonasmue/cover-letter-gpt2", return_dict=True)
-model.to('cuda')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
 
 # Queue 핸들링
 def handle_requests_by_batch():
@@ -28,42 +29,89 @@ def handle_requests_by_batch():
                 continue
 
             for requests in requests_batch:
-                requests['output'] = run(requests['input'][0], requests['input'][1])
-
+                if len(requests['input']) == 2 :
+                    requests['output'] = run_word(requests['input'][0], requests['input'][1])
+                elif len(requests['input']) == 3 :
+                    requests['output'] = run_generation(requests['input'][0], requests['input'][1], requests['input'][2])
 
 # 쓰레드
 threading.Thread(target=handle_requests_by_batch).start()
 
 
+def run_generation(sequence, num_samples, length):
+    try:
+        sequence = sequence.strip()
+        input_ids = tokenizer.encode(sequence, return_tensors='pt')
+
+        # input_ids also need to apply gpu device!
+        input_ids = input_ids.to(device)
+
+        min_length = len(input_ids.tolist()[0])
+        length += min_length
+
+        sample_outputs = model.generate(input_ids, pad_token_id=50256,
+                                        do_sample=True,
+                                        max_length=length,
+                                        min_length=length,
+                                        top_k=40,
+                                        num_return_sequences=num_samples)
+
+        result = dict()
+        for idx, sample_output in enumerate(sample_outputs):
+            result[idx] = tokenizer.decode(sample_output.tolist()[min_length:], skip_special_tokens=True)
+
+        return result
+
+    except Exception as e:
+        print(e)
+        return 500
+
+
 # Running GPT-2
-def run(sequence, num_samples):
-    input_ids = tokenizer.encode(sequence, return_tensors="pt")
-    tokens_tensor = input_ids.to('cuda')
-    
-    # get logits of last hidden state
-    next_token_logits = model(tokens_tensor).logits[:, -1, :]
-    # filter
-    filtered_next_token_logits = top_k_top_p_filtering(next_token_logits, top_k=50, top_p=1.0)
-    # sample
-    probs = F.softmax(filtered_next_token_logits, dim=-1)
-    next_token = torch.multinomial(probs, num_samples=num_samples)
+def run_word(sequence, num_samples):
+    try:
+        input_ids = tokenizer.encode(sequence, return_tensors="pt")
+        tokens_tensor = input_ids.to(device)
 
-    result = dict()
-    for idx, token in enumerate(next_token.tolist()[0]):
-        result[idx] = tokenizer.decode(token)
-    return result
+        # get logits of last hidden state
+        next_token_logits = model(tokens_tensor).logits[:, -1, :]
+        # filter
+        filtered_next_token_logits = top_k_top_p_filtering(next_token_logits, top_k=50, top_p=1.0)
+        # sample
+        probs = F.softmax(filtered_next_token_logits, dim=-1)
+        next_token = torch.multinomial(probs, num_samples=num_samples)
+
+        result = dict()
+        for idx, token in enumerate(next_token.tolist()[0]):
+            result[idx] = tokenizer.decode(token)
+
+        return result
+
+    except Exception as e:
+        print(e)
+        return 500
 
 
-@app.route("/gpt2-cover-word", methods=['POST'])
-def gpt2():
+@app.route("/gpt2-cover-letter/<type>", methods=['POST'])
+def gpt2_cover_letter(type):
+    if type != 'short' and type != 'long' :
+        return jsonify({'error': 'This is the wrong address.'}), 400
+
     # 큐에 쌓여있을 경우,
     if requests_queue.qsize() > BATCH_SIZE:
-        return jsonify({'error': 'TooManyReqeusts'}), 429
+        return jsonify({'error': 'Too Many Requests'}), 429
 
-    # 웹페이지로부터 이미지와 스타일 정보를 얻어옴.
     try:
+        args = []
         text = request.form['text']
         num_samples = int(request.form['num_samples'])
+
+        args.append(text)
+        args.append(num_samples)
+
+        if type == 'long':
+            length = int(request.form['length'])
+            args.append(length)
 
     except Exception:
         print("Empty Text")
@@ -71,7 +119,7 @@ def gpt2():
 
     # Queue - put data
     req = {
-        'input': [text, num_samples]
+        'input': args
     }
     requests_queue.put(req)
 
