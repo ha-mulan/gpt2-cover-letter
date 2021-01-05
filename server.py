bdca0231d9d7fadfa168846cba264b38b960c0c8
ha-mulan <54228425+ha-mulan@users.noreply.github.com>
2021-01-05 18:35:49 +0900

    Update server.py

--- server.py
+++ server.py
@@ -29,19 +29,20 @@ def handle_requests_by_batch():
                 continue
 
             for requests in requests_batch:
-                if len(requests['input']) == 2 :
-                    requests['output'] = run_word(requests['input'][0], requests['input'][1])
-                elif len(requests['input']) == 3 :
-                    requests['output'] = run_generation(requests['input'][0], requests['input'][1], requests['input'][2])
+                # requests['output'] = run_word(requests['input'][0], requests['input'][1])
+                # if len(requests['input']) == 2 :
+                # elif len(requests['input']) == 3 :
+                # requests['output'] = run_model(requests['input'][0][0], requests['input'][0][1], requests['input'][0][2])
+                requests['output'] = run_model(requests['input'][0])#[0], requests['input'][0][1], requests['input'][0][2])
+
 
 # 쓰레드
 threading.Thread(target=handle_requests_by_batch).start()
 
-
-def run_generation(sequence, num_samples, length):
+def run_model(prompt, num=10, length=30):
     try:
-        sequence = sequence.strip()
-        input_ids = tokenizer.encode(sequence, return_tensors='pt')
+        prompt = prompt.strip()
+        input_ids = tokenizer.encode(prompt, return_tensors='pt')
 
         # input_ids also need to apply gpu device!
         input_ids = input_ids.to(device)
@@ -49,81 +50,53 @@ def run_generation(sequence, num_samples, length):
         min_length = len(input_ids.tolist()[0])
         length += min_length
 
+        # model = models[model_name]
         sample_outputs = model.generate(input_ids, pad_token_id=50256,
                                         do_sample=True,
                                         max_length=length,
                                         min_length=length,
                                         top_k=40,
-                                        num_return_sequences=num_samples)
+                                        num_return_sequences=num)
 
-        result = dict()
-        for idx, sample_output in enumerate(sample_outputs):
-            result[idx] = tokenizer.decode(sample_output.tolist()[min_length:], skip_special_tokens=True)
+        generated_texts = ""
+        for i, sample_output in enumerate(sample_outputs):
+            output = tokenizer.decode(sample_output.tolist()[
+                                      min_length:], skip_special_tokens=True)
+            generated_texts+= output
 
-        return result
-
-    except Exception as e:
-        print(e)
-        return 500
-
-
-# Running GPT-2
-def run_word(sequence, num_samples):
-    try:
-        input_ids = tokenizer.encode(sequence, return_tensors="pt")
-        tokens_tensor = input_ids.to(device)
-
-        # get logits of last hidden state
-        next_token_logits = model(tokens_tensor).logits[:, -1, :]
-        # filter
-        filtered_next_token_logits = top_k_top_p_filtering(next_token_logits, top_k=50, top_p=1.0)
-        # sample
-        probs = F.softmax(filtered_next_token_logits, dim=-1)
-        next_token = torch.multinomial(probs, num_samples=num_samples)
-
-        result = dict()
-        for idx, token in enumerate(next_token.tolist()[0]):
-            result[idx] = tokenizer.decode(token)
-
-        return result
+        return generated_texts
 
     except Exception as e:
         print(e)
         return 500
 
 
-@app.route("/gpt2-cover-letter/<type>", methods=['POST'])
-def gpt2_cover_letter(type):
-    if type != 'short' and type != 'long' :
-        return jsonify({'error': 'This is the wrong address.'}), 400
 
-    # 큐에 쌓여있을 경우,
+# @app.route("/gpt2-recipes-maker/", methods=['GET'])
+@app.route("/api/", methods=['GET'])
+def generate():
+
     if requests_queue.qsize() > BATCH_SIZE:
         return jsonify({'error': 'Too Many Requests'}), 429
 
     try:
         args = []
-        text = request.form['text']
-        num_samples = int(request.form['num_samples'])
 
-        args.append(text)
-        args.append(num_samples)
+        food=request.args.get('food')
+        # num=request.args.get('num')
+        # length=request.args.get('length')
 
-        if type == 'long':
-            length = int(request.form['length'])
-            args.append(length)
+        args.append(food)#,num,length)
 
     except Exception:
         print("Empty Text")
         return Response("fail", status=400)
 
-    # Queue - put data
     req = {
         'input': args
     }
     requests_queue.put(req)
 
-    # Queue - wait & check
     while 'output' not in req:
         time.sleep(CHECK_INTERVAL)
 
